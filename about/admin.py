39bc527bec384648190b14dae9dab2a869fc0c75
DBonbon <45122390+DBonbon@users.noreply.github.com>
2021-07-07 01:22:39 +0200

    Delete about directory

--- about/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import AboutPage
-class AboutPageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'subtitle', 'link', 'contact_title', 'image')
-admin.site.register(AboutPage, AboutPageAdmin)
-
--- about/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from about.views import AboutUs
-urlpatterns = [
-    path('',AboutUs.as_view(),name="about"),
-]
