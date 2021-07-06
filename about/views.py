8fb60d74d07a102a7f1a4125d3130404cdae618a
DBonbon <45122390+DBonbon@users.noreply.github.com>
2021-07-07 01:07:35 +0200

    Update views.py

--- about/views.py
+++ about/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
-
-def about_page(request):
-    return render(request, 'about/about_page.html')
+from django.views.generic import ListView
+from about.models import AboutPage
+from django.views import generic
+from django.views.generic.base import TemplateView
+class AboutUs(TemplateView):
+    model = AboutPage
+    # context_object_name = 'about'
+    template_name = 'about/aboutus.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['aboutpages'] = AboutPage.objects.get(id=1)
+        return context
