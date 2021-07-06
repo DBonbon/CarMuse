c263e82ff718e3bb012a31c802734d9dfccb0b6c
DBonbon <45122390+DBonbon@users.noreply.github.com>
2021-07-07 01:05:40 +0200

    Update admin.py

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AboutPage
+class AboutPageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'subtitle', 'link', 'contact_title', 'image')
+admin.site.register(AboutPage, AboutPageAdmin)
 
-# Register your models here.
